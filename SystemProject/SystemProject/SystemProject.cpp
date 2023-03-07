6270f78944cd6eba2575934184d4e0cfba7fc129
성원 <47082317+Ho-rang@users.noreply.github.com>
2023-03-07 20:33:07 +0900

    ini 파일 읽어오는중
    경로는 맞는데 GetPrivateProfileStringA로 읽어오지 못한다?

--- SystemProject/SystemProject/SystemProject.cpp
+++ SystemProject/SystemProject/SystemProject.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
+#include <Windows.h>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const std::string filename = "setting.ini";
+
+	const std::string section = "JSON";
+	const std::string key = "derectory";
+
+	char value[256];
+	GetPrivateProfileStringA(section.c_str(), key.c_str(), "", value, 256, filename.c_str());
+
+	std::cout << "Value : " << value << std::endl;
+
+
+    return 0;
 }
